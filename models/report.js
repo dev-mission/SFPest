d08efadae89f5e0224346e0a12f3522afded84b7
Ajmal Shah <ajmalshah10@gmail.com>
2018-10-27 16:34:03 -0700

    Created a new model titled Report in the database

---
+++ models/report.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Report = sequelize.define('Report', {
+    what: DataTypes.STRING,
+    where: DataTypes.TEXT,
+    pictureUrl: DataTypes.STRING,
+    name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    streetNumber: DataTypes.STRING,
+    streetName: DataTypes.STRING,
+    unitNumber: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    postalCode: DataTypes.STRING
+  }, {});
+  Report.associate = function(models) {
+    // associations can be defined here
+  };
+  return Report;
+};
