776be83aec20e050361fab5e719df43b22ec814c
Daniel Guardado <dguardado901@gmail.com>
2018-11-08 13:41:04 -0800

    Made a new route and view for submissions.

--- routes/submissions.js
+++ routes/submissions.js
@@ -1,16 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET First Part of Submission Form page. */
-router.get('/', function(req, res, next) {
+router.get('/step1', function(req, res, next) {
   res.render('submissions/new', { title: 'New Pest Report' });
 });
 
 /* Post Second part of Submission Form page. */
-router.post('/', function(req, res, next) {
+router.post('/step2', function(req, res, next) {
   res.render('submissions/new-users', {
     title: 'Your Info',
     step1: req.body
+  });
+});
+
+/* Post to create a new report. */
+router.post('/', function(req, res, next) {
+  models.Report.create({
+    what: req.body.what,
+    where: req.body.where,
+    pictureUrl: req.body.pictureUrl,
+    name: req.body.name,
+    phone: req.body.phone,
+    streetNumber: req.body.streetNumber,
+    streetName: req.body.streetName,
+    unitNumber: req.body.unitNumber,
+    city: "San Francisco",
+    state: "California",
+    postalCode: "94124",
+  }).then(function(report) {
+    res.render(`submissions/thank-you`, {
+      title: 'Thank You',
+      report: report
+    });
   });
 });
 
