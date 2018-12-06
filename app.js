503da0b94042b0005e40f39aeaa79aa5c141eeb8
Daniel Guardado <dguardado901@gmail.com>
2018-12-06 14:33:37 -0800

    Added log in routes and session libraries.

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 var submissionsRouter = require('./routes/submissions');
 var reportsRouter = require('./routes/reports');
 var registerRouter = require('./routes/register');
+var loginRouter = require('./routes/login');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
 
 var app = express();
 
@@ -20,14 +24,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'changeme'}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/libraries/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+app.use(function(req,res,next){
+  res.locals.flash = req.flash();
+  res.locals.current_user = req.user;
+  next();
+});
 
 app.use('/submissions', submissionsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reports', reportsRouter);
 app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+var bcrypt = require('bcrypt')
+/* GET New User Form. */
+router.get('/', function(req, res, next) {
+    res.render('login/new', { title: 'Log In' });
+  });
+
+  /* Post to create a new report. */
+router.post('/', function(req, res, next) {
+    bcrypt.hash(req.body.password, 10).then(function(hashed_password) {
+        models.User.create({
+            username: req.body.username,
+            hashed_password: hashed_password
+        }).then(function(report) {
+          res.redirect('/');
+        });
+    }); 
+});
+  
+
+module.exports = router;
