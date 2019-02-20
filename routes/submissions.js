c3b9e7d51fb2a0fb5b7eacaf84e2b41090116597
Francis Li <mail@francisli.com>
2019-02-20 13:34:05 -0800

    Refactor photo upload

--- routes/submissions.js
+++ routes/submissions.js
@@ -12,16 +12,8 @@ router.get('/step1', function(req, res, next) {
   res.render('submissions/new', { title: 'New Pest Report' });
 });
 
-/* Post Second part of Submission Form page. */
-router.post('/step2', function(req, res, next) {
-  //// set up a helper function to render
-  const render = function() {
-    res.render('submissions/new-users', {
-      title: 'Your Info',
-      step1: req.body
-    });
-  }
-  //// if photo submitted, copy to a temporary location
+/** Helper function to check for a picture upload */
+function handlePictureFromStep1(req, callback) {
   if (req.files && req.files.picture) {
     const pictureUrl = `/uploads/${uuid()}/original.${mime.extension(req.files.picture.mimetype)}`;
     //// in production, upload to S3
@@ -33,23 +25,44 @@ router.post('/step2', function(req, res, next) {
       mkdirp.sync(path.dirname(dest));
       req.files.picture.mv(dest, function(err) {
         req.body.pictureUrl = pictureUrl;
-        render();
+        callback();
       });
     }
   } else {
-    //// just render directly
-    render();
+    callback();
   }
+}
+
+/* POST to Second part of Submission Form page. */
+router.post('/step2', function(req, res, next) {
+  handlePictureFromStep1(req, function () {
+    res.render('submissions/new-users', {
+      title: 'Your Info',
+      step1: req.body
+    });
+  })
 });
+
+function handlePictureFromStep2(report, callback) {
+  if (report.pictureUrl && report.pictureUrl != "") {
+    //// if a picture was uploaded, move to a permanent location
+    if (process.env.NODE_ENV == 'production') {
+      //// TODO
+    } else {
+      //// move into public uploads
+      const src = `/tmp${report.pictureUrl}`;
+      const dest = `${path.resolve(__dirname, '../public')}${report.pictureUrl}`;
+      mv(src, dest, {mkdirp: true}, function(err) {
+        callback();
+      });
+    }
+  } else {
+    callback();
+  }
+}
 
 /* Post to create a new report. */
 router.post('/', function(req, res, next) {
-  const render = function(report) {
-    res.render(`submissions/thank-you`, {
-      title: 'Thank You',
-      report: report
-    });
-  };
   models.Report.create({
     what: req.body.what,
     location: req.body.location,
@@ -63,21 +76,12 @@ router.post('/', function(req, res, next) {
     state: "California",
     postalCode: "94124",
   }).then(function(report) {
-    if (req.body.pictureUrl) {
-      //// if a picture was uploaded, move to a permanent location and update
-      if (process.env.NODE_ENV == 'production') {
-        //// TODO
-      } else {
-        //// move into public uploads
-        const src = `/tmp${req.body.pictureUrl}`;
-        const dest = `${path.resolve(__dirname, '../public')}${req.body.pictureUrl}`;
-        mv(src, dest, {mkdirp: true}, function(err) {
-          render(report);
-        });
-      }
-    } else {
-      render(report);
-    }
+    handlePictureFromStep2(report, function() {
+      res.render(`submissions/thank-you`, {
+        title: 'Thank You',
+        report: report
+      });      
+    })
   });
 });
 
